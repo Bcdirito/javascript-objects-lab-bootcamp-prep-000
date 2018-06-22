2298bacfab14fc475b9028bb313676aa9121b296
bdirito91@gmail.com <bdirito91@gmail.com>
2018-06-22 04:08:28 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var recipes = {
   cheese: 'mozzarella'
 };
 function updateObjectWithKeyAndValue(obj, key, value) {
- return Object.assign({}, obj, { [key]: value });
+ return Object.assign({}, recipes, { ['prop']: 1}, { ['prop2']: 2});
 }
 function destructivelyUpdateObjectWithKeyAndValue(obj, key, value) {
   obj[key] = value;

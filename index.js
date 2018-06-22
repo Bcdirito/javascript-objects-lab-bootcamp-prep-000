764767e0db92ddbae48edb99cf4cd46c0d027dd6
bdirito91@gmail.com <bdirito91@gmail.com>
2018-06-22 04:12:43 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,6 +15,6 @@ function deleteFromObjectByKey(object, key) {
   return recipes;
 }
 function destructivelyDeleteFromObjectByKey(object, key) {
-  var newObj = Object.assign({}, object, prop);
+  var newObj = Object.assign({}, object, 'prop');
   delete newObj.prop;
 }
